e6dfe3488b82c7653f5945db8a87bf5af2b321bb
Fredcapit <shibanov.fv@gmail.com>
2018-03-03 22:11:58 +0300

    First commit

---
+++ p5/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace p5
+{
+    public interface PartialContentProducerInerface
+    {
+
+        Task isAvailable();
+        Task IsRangeHeaderExist();
+        Task CanRangeBeFulfilled();
+        Task ProduceConent();
+    }
+}
